82d440b88f7f2af604087a53959b1b15dc1e170a
Esi're <esiri92@gmail.com>
2023-08-03 21:52:38 +0000

    chore: add plugin to version static assets.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import path from 'path';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,38 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  plugins: [
+    htmlCacheBusterPlugin([
+      'webphone.js',
+      'widget.js',
+      'style.css',
+    ]),
+  ],
 });
+
+function htmlCacheBusterPlugin(assets: string[] = []): PluginOption {
+  // const regex = /(?<=(?:src|href)=")(?!https?)(.*?\.\w{2,4})(?=")/g;
+  const assetsRegex = new RegExp(`(?<=(?:src|href)=")(?!https?)(.*?(?:${assets.join('|')}))(?=")`, 'g');
+
+  return {
+    name: 'html:static-asset-versioning',
+    apply: 'build',
+    enforce: 'post',
+    transformIndexHtml: (html, ctx) => {
+      const now = Date.now();
+
+      return html.replace(assetsRegex, substring => {
+        const path = substring.replace('.', 'public');
+
+        if (!fs.existsSync(path)) {
+          console.log('asset not found!', { substring, path });
+          return `${substring}?v=${now}`;
+        }
+
+        const stats = fs.statSync(path);
+
+        return `${substring}?v=${stats.mtime.valueOf()}`;
+      });
+    },
+  };
+}
